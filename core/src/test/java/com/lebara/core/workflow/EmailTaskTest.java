182a31ecd4d8b656173b63663966a2176f8357af
ADOBENET\vsah <vsah@adobe.com>
2021-06-05 19:19:01 +0530

    removed redundant components

--- core/src/test/java/com/lebara/core/workflow/EmailTaskTest.java
+++ core/src/test/java/com/lebara/core/workflow/EmailTaskTest.java
@@ -2,7 +2,9 @@ package com.lebara.core.workflow;
 
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith({AemContextExtension.class})
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
 public class EmailTaskTest {
+
 }
