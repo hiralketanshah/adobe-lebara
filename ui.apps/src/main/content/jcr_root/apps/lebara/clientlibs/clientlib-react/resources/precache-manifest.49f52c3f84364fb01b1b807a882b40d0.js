585c0cf89de3bcb9da1790354765d413ab25d0c3
ADOBENET\ankumari <ankumari@adobe.com>
2021-07-12 15:56:05 +0530

    dev work

---
+++ ui.apps/src/main/content/jcr_root/apps/lebara/clientlibs/clientlib-react/resources/precache-manifest.49f52c3f84364fb01b1b807a882b40d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfb8a7ec83f2dbbbbe429a3085b94fe",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "0027e1596e51e7bff808",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/css/4.9c303500.chunk.css"
+  },
+  {
+    "revision": "0ef90f0874756c73a87c",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/css/main.01227443.chunk.css"
+  },
+  {
+    "revision": "3cfeb6f1ee8f79298a4b",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/2.37439fd3.chunk.js"
+  },
+  {
+    "revision": "a112985e346e36813f8faa2e1e345c78",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/2.37439fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8ffeb4c06a74c7b7bd",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/3.60409437.chunk.js"
+  },
+  {
+    "revision": "c1a19abb78cf9cde35b8b3bd4cf9adc0",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/3.60409437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0027e1596e51e7bff808",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/4.4c08ba5f.chunk.js"
+  },
+  {
+    "revision": "c61b2851e7a2d47c50e8",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/5.f1a202cc.chunk.js"
+  },
+  {
+    "revision": "ef32295dc71e0113617e",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/6.0a009567.chunk.js"
+  },
+  {
+    "revision": "0ef90f0874756c73a87c",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/main.971cd5a9.chunk.js"
+  },
+  {
+    "revision": "54008f994b329d233eaa",
+    "url": "/etc.clientlibs/lebara/clientlibs/clientlib-react/resources/static/js/runtime-main.4954b472.js"
+  }
+]);
