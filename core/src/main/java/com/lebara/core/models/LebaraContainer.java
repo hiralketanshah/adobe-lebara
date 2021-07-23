c8dfbc70f86df1c59bea9ad2af053d9adef3a1f4
Manigandan Devaraj <devaraj@adobe.com>
2021-07-23 23:22:30 +0530

    [WP-671] Cleanup

--- core/src/main/java/com/lebara/core/models/LebaraContainer.java
+++ core/src/main/java/com/lebara/core/models/LebaraContainer.java
@@ -1,7 +1,6 @@
 package com.lebara.core.models;
 
 import com.adobe.cq.wcm.core.components.models.LayoutContainer;
-import com.drew.lang.annotations.Nullable;
 import org.osgi.annotation.versioning.ConsumerType;
 
 @ConsumerType
