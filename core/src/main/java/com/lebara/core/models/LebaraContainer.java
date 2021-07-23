e936a96a60ae79a48c91cdaef3cebf1278d860cb
Manigandan Devaraj <devaraj@adobe.com>
2021-07-23 23:15:03 +0530

    [WP-671] Container component custom implementation for sling exporter

---
+++ core/src/main/java/com/lebara/core/models/LebaraContainer.java
@@ -0,0 +1,10 @@
+package com.lebara.core.models;
+
+import com.adobe.cq.wcm.core.components.models.LayoutContainer;
+import com.drew.lang.annotations.Nullable;
+import org.osgi.annotation.versioning.ConsumerType;
+
+@ConsumerType
+public interface LebaraContainer extends LayoutContainer {
+    // Empty Interface for Containers
+}
