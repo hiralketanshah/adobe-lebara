fc0c4f64415f3dc32e97a8436b710211dfb1a66a
ADOBENET\vsah <vsah@adobe.com>
2021-08-07 23:37:37 +0530

    modified typescript code

--- ui.frontend/src/components/FooterCopyright/FooterCopyright.tsx
+++ ui.frontend/src/components/FooterCopyright/FooterCopyright.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FooterCopyrightProps } from "./types";
 
-const FooterCopyright : React.FC<FooterCopyrightProps> = ({ copyrightInfo }, copyrightText) => (
-<> 
-
-    {copyrightInfo.map(item => <> <>{item.label}</> <>{item.link}</> </>)}
-    
-</>
-);
+const FooterCopyright : React.FC<FooterCopyrightProps> = ({ copyrightInfo, copyrightText } ) => {
+    return (
+        <div> 
+            {copyrightInfo.map(item => <div> label : {item.label}  link : {item.link} </div>)}
+            <div>copyrightText is {copyrightText}</div>
+        </div>
+    );
+};
 
 export default FooterCopyright;
