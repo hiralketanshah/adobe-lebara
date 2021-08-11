7139436358bebce7d0de6169e2a4001a2b464ccc
Krishna Hingu <wip50706@adobe.com>
2021-08-11 15:51:07 +0530

    header menu font family changed to roboto

--- ui.frontend/src/index.js
+++ ui.frontend/src/index.js
@@ -1,38 +1,42 @@
-import 'react-app-polyfill/stable';
-import 'react-app-polyfill/ie9';
-import 'custom-event-polyfill';
+import "react-app-polyfill/stable";
+import "react-app-polyfill/ie9";
+import "custom-event-polyfill";
 
-import { Constants, ModelManager } from '@adobe/aem-spa-page-model-manager';
-import { createBrowserHistory } from 'history';
-import React from 'react';
-import { render } from 'react-dom';
-import { Router } from 'react-router-dom';
-import App from './App';
-import './components/import-components';
-import './index.css';
+import { Constants, ModelManager } from "@adobe/aem-spa-page-model-manager";
+import { createBrowserHistory } from "history";
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "react-router-dom";
+import App from "./App";
+import "./components/import-components";
+import "./index.css";
+
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 
 const renderApp = () => {
-    ModelManager.initialize().then(pageModel => {
-        const history = createBrowserHistory();
-        render(
-            <Router history={history}>
-                <App
-                    history={history}
-                    cqChildren={pageModel[Constants.CHILDREN_PROP]}
-                    cqItems={pageModel[Constants.ITEMS_PROP]}
-                    cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
-                    cqPath={pageModel[Constants.PATH_PROP]}
-                    locationPathname={window.location.pathname}
-                />
-            </Router>,
-            document.getElementById('spa-root')
-        );
-    });
+  ModelManager.initialize().then((pageModel) => {
+    const history = createBrowserHistory();
+    render(
+      <Router history={history}>
+        <App
+          history={history}
+          cqChildren={pageModel[Constants.CHILDREN_PROP]}
+          cqItems={pageModel[Constants.ITEMS_PROP]}
+          cqItemsOrder={pageModel[Constants.ITEMS_ORDER_PROP]}
+          cqPath={pageModel[Constants.PATH_PROP]}
+          locationPathname={window.location.pathname}
+        />
+      </Router>,
+      document.getElementById("spa-root")
+    );
+  });
 };
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    renderApp();
+document.addEventListener("DOMContentLoaded", () => {
+  renderApp();
 });
