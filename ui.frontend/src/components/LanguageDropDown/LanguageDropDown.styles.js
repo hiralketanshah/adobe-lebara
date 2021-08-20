eb65b66e904185002dd035a2034b2b613ab031c3
Krishna Hingu <wip50706@adobe.com>
2021-08-20 11:41:26 +0530

    Language padding issue fixed

--- ui.frontend/src/components/LanguageDropDown/LanguageDropDown.styles.js
+++ ui.frontend/src/components/LanguageDropDown/LanguageDropDown.styles.js
@@ -5,6 +5,9 @@ import Select from "../Select/Select";
 export const SelectWrapper = styled(Select)`
   ${mediaquery.desktop`
   height: 32px !important;
+  `}
+  ${mediaquery.tablet`
+    height:32px !important;
 `}
   ${mediaquery.mobile`
     height:49px !important;
