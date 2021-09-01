50abe73543dbfda412a753d085981687473cdeeb
Krishna Hingu <wip50706@adobe.com>
2021-09-02 00:28:59 +0530

    fixed language dropdown style and country flag imags size

--- ui.frontend/src/components/CountryWithFlag/CountryWithFlag.tsx
+++ ui.frontend/src/components/CountryWithFlag/CountryWithFlag.tsx
@@ -9,7 +9,7 @@ const CountryWithFlag: React.FC<CountryWithFlagProps> = ({
   countryCode,
 }) => (
   <Flex alignItems="center" gridGap={2} fontSize={12}>
-    <img src={countryFlag} alt={countryCode} />
+    <img src={countryFlag} alt={countryCode} height="11" width="16" />
     <Title type="caption">{countryName}</Title>
   </Flex>
 );
