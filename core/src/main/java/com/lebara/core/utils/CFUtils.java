c013b6def33e65e1ecfd47376e3b5a6c05446c45
Manigandan Devaraj <devaraj@adobe.com>
2021-09-03 21:31:21 +0530

    Corrected the format

--- core/src/main/java/com/lebara/core/utils/CFUtils.java
+++ core/src/main/java/com/lebara/core/utils/CFUtils.java
@@ -16,54 +16,54 @@ import com.lebara.core.dto.OfferFragmentBean;
 
 public class CFUtils {
 
-	private CFUtils() {
-		// Private Constructor
-	}
+    private CFUtils() {
+        // Private Constructor
+    }
 
-	public static <T> List<T> convertStringArrayToList(String[] stringArray, Class<T> T) {
-		Gson gson = new Gson();
-		return Arrays.stream(stringArray).map(al -> gson.fromJson(al, T)).collect(Collectors.toList());
-	}
+    public static <T> List<T> convertStringArrayToList(String[] stringArray, Class<T> T) {
+        Gson gson = new Gson();
+        return Arrays.stream(stringArray).map(al -> gson.fromJson(al, T)).collect(Collectors.toList());
+    }
 
-	public static String getElementValue(ContentFragment cf, String elementName) {
-		return cf.getElement(elementName) == null ? StringUtils.EMPTY : cf.getElement(elementName).getContent();
-	}
+    public static String getElementValue(ContentFragment cf, String elementName) {
+        return cf.getElement(elementName) == null ? StringUtils.EMPTY : cf.getElement(elementName).getContent();
+    }
 
-	public static String[] getElementArrayValue(ContentFragment cf, String elementName) {
-		return cf.getElement(elementName) == null ? new String[0]
-				: cf.getElement(elementName).getValue().getValue(String[].class);
-	}
+    public static String[] getElementArrayValue(ContentFragment cf, String elementName) {
+        return cf.getElement(elementName) == null ? new String[0]
+                : cf.getElement(elementName).getValue().getValue(String[].class);
+    }
 
-	public static List<OfferFragmentBean> getCfDetails(String cfPath, ResourceResolver resourceResolver) {
-		List<OfferFragmentBean> resultList = new ArrayList<>();
-		if (StringUtils.isNotBlank(cfPath)) {
-			Resource cfResource = resourceResolver.getResource(cfPath);
-			OfferFragmentBean offerFragmentBean=populateOffers(cfResource);
-			if(offerFragmentBean !=null)
-			resultList.add(offerFragmentBean);
+    public static List<OfferFragmentBean> getCfDetails(String cfPath, ResourceResolver resourceResolver) {
+        List<OfferFragmentBean> resultList = new ArrayList<>();
+        if (StringUtils.isNotBlank(cfPath)) {
+            Resource cfResource = resourceResolver.getResource(cfPath);
+            OfferFragmentBean offerFragmentBean=populateOffers(cfResource);
+            if(offerFragmentBean !=null)
+            resultList.add(offerFragmentBean);
 
-		}
-		return resultList;
-	}
+        }
+        return resultList;
+    }
 
-	public static OfferFragmentBean populateOffers( Resource cfResource) {
-		OfferFragmentBean offerFragmentBean=null;
-		if (null != cfResource) {
-			ContentFragment offerFragment = cfResource.adaptTo(ContentFragment.class);
-			if (null != offerFragment) {
-				 offerFragmentBean = new OfferFragmentBean();
-				offerFragmentBean.setCost(CFUtils.getElementValue(offerFragment, "cost"));
-				offerFragmentBean.setValidity(CFUtils.getElementValue(offerFragment, "validity"));
-				if (offerFragment.getElement("allowancesList") != null) {
-					String[] allowanceArray = CFUtils.getElementArrayValue(offerFragment, "allowancesList");
-					List<CFAllowance> allowanceList = CFUtils.convertStringArrayToList(allowanceArray,
-							CFAllowance.class);
-					offerFragmentBean.setAllowanceList(allowanceList);
-				}
-				
-			}
-		}
-		return offerFragmentBean;
-	}
+    public static OfferFragmentBean populateOffers( Resource cfResource) {
+        OfferFragmentBean offerFragmentBean = null;
+        if (null != cfResource) {
+            ContentFragment offerFragment = cfResource.adaptTo(ContentFragment.class);
+            if (null != offerFragment) {
+                offerFragmentBean = new OfferFragmentBean();
+                offerFragmentBean.setCost(CFUtils.getElementValue(offerFragment, "cost"));
+                offerFragmentBean.setValidity(CFUtils.getElementValue(offerFragment, "validity"));
+                if (offerFragment.getElement("allowancesList") != null) {
+                    String[] allowanceArray = CFUtils.getElementArrayValue(offerFragment, "allowancesList");
+                    List<CFAllowance> allowanceList = CFUtils.convertStringArrayToList(allowanceArray,
+                            CFAllowance.class);
+                    offerFragmentBean.setAllowanceList(allowanceList);
+                }
+                
+            }
+        }
+        return offerFragmentBean;
+    }
 
 }
