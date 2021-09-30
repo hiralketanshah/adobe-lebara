d1ce0b1343b71b70dd2945e36c8017637984da3d
vishal.sah <vishal.sah@lebara.com>
2021-09-30 22:16:01 +0530

    v

--- core/src/main/java/com/lebara/core/models/PersonalDetailsExporter.java
+++ core/src/main/java/com/lebara/core/models/PersonalDetailsExporter.java
@@ -25,12 +25,12 @@ import java.util.ResourceBundle;
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {ViewPlanExporter.class, ComponentExporter.class},
         resourceType = ViewPlanExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class ViewPlanExporter implements ComponentExporter {
+public class PersonalDetailsExporter implements ComponentExporter {
 
     /**
      * The resource type.
      */
-    protected static final String RESOURCE_TYPE = "lebara/components/viewplans";
+    protected static final String RESOURCE_TYPE = "lebara/components/personaldetails";
 
     @SlingObject
     private ResourceResolver resourceResolver;
@@ -41,44 +41,6 @@ public class ViewPlanExporter implements ComponentExporter {
     @SlingObject
     private SlingHttpServletRequest slingRequest;
 
-    @ChildResource
-    protected Resource phases;
-
-    @ValueMapValue
-    private String buttonLabel;
-
-    @ValueMapValue
-    private String minutesField;
-
-    @ValueMapValue
-    private String unlimitedTextField;
-
-    private I18n i18n;
-
-    @PostConstruct
-    private void init() {
-        i18n = AemUtils.geti18n(resourceResolver, resource, slingRequest);
-    }
-
-    public String getButtonLabel() {
-        return buttonLabel;
-    }
-
-    public String getMinutesField() {
-        return minutesField;
-    }
-
-    public String getUnlimitedTextField() {
-        return unlimitedTextField;
-    }
-
-    public List<OfferFragmentBean> getOffers() {
-        List<OfferFragmentBean> offers = new ArrayList<>();
-        if (null != phases) {
-            offers = CFUtils.getCfList(phases, resourceResolver, i18n);
-        }
-        return offers;
-    }
 
     @Override
     public String getExportedType() {
