fc6ddba1806e887351a8890e8a2897f03e86bc01
vishal.sah <vishal.sah@lebara.com>
2021-09-30 23:52:38 +0530

    added be changes

---
+++ core/src/main/java/com/lebara/core/models/ReviewExporter.java
@@ -0,0 +1,33 @@
+package com.lebara.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = SlingHttpServletRequest.class, adapters = {ReviewExporter.class, ComponentExporter.class},
+        resourceType = ReviewExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class ReviewExporter implements ComponentExporter {
+
+    /**
+     * The resource type.
+     */
+    protected static final String RESOURCE_TYPE = "lebara/components/review";
+
+    @ValueMapValue
+    private String title;
+
+    public String getTitle() {
+        return StringUtils.isBlank(title) ? "What Customers Say" : title;
+    }
+
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
+}
