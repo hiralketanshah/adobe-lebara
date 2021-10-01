fa83de44155764872baf9b77c0eb03878dcacf31
Md Khalid <kahlid@adobe.com>
2021-10-01 20:04:12 +0530

    Changed folder names AboutLebara, Usp and Faq component

---
+++ ui.frontend/src/components/AboutLebara/TextFeature.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { AboutLebaraProps } from "./types";
+import TextBlock from "../TextBlock/TextBlock";
+import { Image } from "../Image/Image";
+
+const TextFeature: React.FC<AboutLebaraProps> = ({
+  pretitle,
+  title,
+  actionsEnabled,
+  actions,
+  description,
+  isFullWidthButton,
+  imagePath,
+  linkURL,
+  buttonStyle
+}) => (
+    <Flex
+      flexDirection={{ base: "column", lg: "row" }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex w={{ md: "50%" }} justifyContent="center">
+        {imagePath && (
+          <Image src={imagePath}  w={{lg: 398 }} alt="About lebara" />
+        )}
+      </Flex>
+      <Spacer />
+      <Flex
+        maxW={{ lg: "50%" }}
+        flexDirection="column"
+        w="100%"
+      >
+        <TextBlock
+          buttonText={actionsEnabled ? actions && actions[0]?.title : ""}
+          linkURL={linkURL ? linkURL : actionsEnabled ? actions && actions[0]?.url : "#"}
+          slogan={pretitle}
+          header={title}
+          subHeader={description}
+          buttonStyle={buttonStyle}
+        />
+      </Flex>
+    </Flex>
+  );
+
+export default TextFeature;
