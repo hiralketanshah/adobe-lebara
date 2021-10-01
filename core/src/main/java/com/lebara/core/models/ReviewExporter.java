4e7f2250929940ba15c9e2c74d247b076c648388
vishal.sah <vishal.sah@lebara.com>
2021-10-01 14:27:09 +0530

    adding changes for review component working

---
+++ core/src/main/java/com/lebara/core/models/ReviewExporter.java
@@ -0,0 +1,34 @@
+package com.lebara.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {SlingHttpServletRequest.class, Resource.class}, adapters = {ReviewExporter.class, ComponentExporter.class},
+        resourceType = ReviewExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class ReviewExporter extends HeadingExporter {
+
+    /**
+     * The resource type.
+     */
+    protected static final String RESOURCE_TYPE = "lebara/components/review";
+
+    @ValueMapValue
+    private String title;
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
+}
