5a8279fd5cda92b67fc5bd71e66fcc14a6de0b1a
ADOBENET\ankumari <ankumari@adobe.com>
2021-10-01 13:34:33 +0530

    USP

--- core/src/main/java/com/lebara/core/models/UspExporter.java
+++ core/src/main/java/com/lebara/core/models/UspExporter.java
@@ -1,7 +1,7 @@
 package com.lebara.core.models;
 
-import java.util.List;
-
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -9,52 +9,32 @@ import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import com.adobe.cq.export.json.ComponentExporter;
-import com.adobe.cq.export.json.ExporterConstants;
+import java.util.List;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = { UspExporter.class,
         ComponentExporter.class }, resourceType = UspExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class UspExporter implements ComponentExporter {
+public class UspExporter extends HeadingExporter {
 
     /**
      * The resource type.
      */
     protected static final String RESOURCE_TYPE = "lebara/components/usp";
 
-    @ValueMapValue
-    private String backgroundimage;
-
-  
-
-    @ValueMapValue
-    private String title;
-
     @ScriptVariable
     private Resource resource;
 
     @ChildResource
-    private List<ImageProperties> uspDescription;
+    private List<ImageProperties> uspList;
 
     @Override
     public String getExportedType() {
         return resource.getResourceType();
     }
 
-    public String getTitle() {
-        return title;
+    public List<ImageProperties> getUspList() {
+        return uspList;
     }
-
-    public String getBackgroundimage() {
-        return backgroundimage;
-    }
-
-  
-    public List<ImageProperties> getUspDescription() {
-        return uspDescription;
-    }
-
 
 }
