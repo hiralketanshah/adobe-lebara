65b9d693a30cfcd6c2563d2d0c4d35aa4f30f3fc
Md Khalid <kahlid@adobe.com>
2021-10-04 16:16:38 +0530

    Update AemConfig.js

--- ui.frontend/src/components/TrustedShopSlider/AemConfig.js
+++ ui.frontend/src/components/TrustedShopSlider/AemConfig.js
@@ -2,7 +2,7 @@ import { MapTo } from "@adobe/aem-react-editable-components";
 import TrustedShopSlider from "./TrustedShopSlider";
 
 const TrustedShopSliderConfig = {
-    emptyLabel: "FAQ",
+    emptyLabel: "Review Component",
     isEmpty: function (props) {
       return !props.title;
     },
