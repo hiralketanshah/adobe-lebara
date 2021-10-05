a4bcc0b01f563fe668c86d8afcac7a6ca60afc80
Md Khalid <kahlid@adobe.com>
2021-10-05 11:20:24 +0530

    Update AemConfig.js

--- ui.frontend/src/components/Carousel/AemConfig.js
+++ ui.frontend/src/components/Carousel/AemConfig.js
@@ -2,7 +2,7 @@ import { MapTo } from "@adobe/aem-react-editable-components";
 import Carousel from "./Carousel";
 
 const CarouselConfig = {
-    emptyLabel: "FAQ",
+    emptyLabel: "Carousel",
     isEmpty: function (props) {
       return !props.cqItems
     },
