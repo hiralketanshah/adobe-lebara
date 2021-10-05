001a3ce9b4fa3b2dbcb4ccab1bc6c8de3a36b920
Md Khalid <kahlid@adobe.com>
2021-10-05 11:21:28 +0530

    Update AemConfig.js

--- ui.frontend/src/components/FooterMenu/AemConfig.js
+++ ui.frontend/src/components/FooterMenu/AemConfig.js
@@ -2,7 +2,7 @@ import { MapTo } from "@adobe/aem-react-editable-components";
 import FooterMenu from "./FooterMenu";
 
 const FooterMenuConfig = {
-    emptyLabel: "FAQ",
+    emptyLabel: "Footer Menu",
     isEmpty: function (props) {
       return !props.desktopFootertext || !props.followus || !props.getapp || !props.copyrightText || !props.copyrightLinks;
     },
