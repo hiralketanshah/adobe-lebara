9b9ad35af284b29062929045475e741a311a5d01
Sundari G <SG00592155@techmahindra.com>
2021-10-22 16:36:29 +0530

    WP-2820[bugfix]optimizing simchoice route component

--- ui.frontend/src/components/SimChoice/SimChoiceRoute.tsx
+++ ui.frontend/src/components/SimChoice/SimChoiceRoute.tsx
@@ -6,32 +6,53 @@ import SelectNumberAndOrderDetailsLayout from "../../layouts/SelectNumberAndOrde
 import { ReduxState } from "../../redux/types";
 import { highlightButton } from "../../redux/actions/highlightActions";
 import { SimChoiceProps } from "./types";
+import { useHistory } from "react-router-dom";
+import useAddToCart from "../../hooks/useAddToCart";
 
 const SimChoiceRoute: React.FC<SimChoiceProps> = ({
   description,
   ctaOneLable,
   ctaTwoLable,
   ctaOneLink,
-  ctaTwoLink
+  ctaTwoLink,
+  isAddFreeSimtoCart
 }) => {
+  const [addItemToCart] = useAddToCart();
+  const history = useHistory();
   const dispatch = useDispatch();
   const highlightedButton = useSelector(
     (state: ReduxState) => state.highlightedButton.key
   );
-  useEffect(
-    () => () => {
+  useEffect(() => {
+    if (isAddFreeSimtoCart) {
+      return history.listen(() => {
+        if (history.action === "POP") {
+          dispatch(
+            highlightButton({
+              key: 1,
+            })
+          );
+        }
+      })
+    }
+    else {
       dispatch(
         highlightButton({
           key: 0,
         })
-      );
-    },
-    [dispatch]
-  );
+      )
+    }
+  }, [dispatch, history]);
 
+  const cartItems = useSelector((state: ReduxState) => state.cart.items);
+  const addtoCart = async () => {
+    if (!cartItems.find((t) => t.id === 99999999)) {
+      await addItemToCart(99999999, "Free Sim", "", 0, "free-sim");
+    }
+  };
   return (
     <SelectNumberAndOrderDetailsLayout>
-      <Box>
+      <Box py="104px">
         <ChoiceButtons
           text={description}
           buttonOptions={[
@@ -39,13 +60,23 @@ const SimChoiceRoute: React.FC<SimChoiceProps> = ({
               key: 0,
               variant: highlightedButton === 0 ? "outline" : "solid",
               children: ctaOneLable,
-              path: ctaOneLink,
+              ...(isAddFreeSimtoCart ? {
+                onClick: async () => {
+                  addtoCart();
+                  history.push(ctaOneLink);
+                }
+              } : { path: ctaOneLink }),
             },
             {
               key: 1,
               children: ctaTwoLable,
               variant: highlightedButton === 1 ? "outline" : "solid",
-              path: ctaTwoLink,
+              ...(isAddFreeSimtoCart ? {
+                onClick: async () => {
+                  addtoCart();
+                  history.push(ctaTwoLink);
+                }
+              } : { path: ctaTwoLink }),
             },
           ]}
         />
