acf8f5df61898e5c01f727330c7283dcc991c007
Jatinder <jatinderbimra@gmail.com>
2021-11-11 16:23:07 +0530

    updates from postpaid-ui and merge

---
+++ ui.frontend/src/components/RichTextModal/types.ts
@@ -0,0 +1,10 @@
+export interface ModalProps {
+  open: any;
+  onClose: any;
+  heading?: string;
+  info?:string;
+  closeLabel?: string;
+  additionalInfo?: string;
+  countryFlagFrom?: string;
+  countryFlagTo?: string;
+}
