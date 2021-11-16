c8e30e2722d9eaebf6dc324f11d323744034c3ec
vishal.sah <vishal.sah@lebara.com>
2021-11-16 17:37:09 +0530

    mani's review comments

--- core/src/main/java/com/lebara/core/beans/PageInfo.java
+++ core/src/main/java/com/lebara/core/beans/PageInfo.java
@@ -1,31 +1,40 @@
 package com.lebara.core.beans;
 
+import com.day.cq.wcm.api.Page;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.lebara.core.models.Link;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class PageInfo {
+@Model(adapters = {PageInfo.class}, adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class PageInfo extends Link {
 
-    private String label;
+    @SlingObject
+    private ResourceResolver resourceResolver;
 
-    private String description;
-
+    @ValueMapValue
     private String link;
 
-    public String getLabel() {
-        return label;
+    private String description;
+    private String title;
+
+    @PostConstruct
+    private void init() {
+        PageInfo pageInfo = new PageInfo();
+        Page page = resourceResolver.getResource(link).adaptTo(Page.class);
+        if (page != null) {
+            description = page.getDescription();
+            title = page.getTitle();
+        }
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
 
     public String getDescription() {
         return description;
@@ -33,5 +42,23 @@ public class PageInfo {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public String getLink() {
+        return link;
+    }
+
+    @Override
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
--- core/src/main/java/com/lebara/core/models/HelpCenterFaqExporter.java
+++ core/src/main/java/com/lebara/core/models/HelpCenterFaqExporter.java
@@ -2,7 +2,6 @@ package com.lebara.core.models;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
-import com.day.cq.wcm.api.Page;
 import com.lebara.core.beans.PageInfo;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -20,7 +19,7 @@ import java.util.List;
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {HelpCenterFaqExporter.class,
         ComponentExporter.class}, resourceType = HelpCenterFaqExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class HelpCenterFaqExporter implements ComponentExporter {
+public class HelpCenterFaqExporter extends HeadingExporter {
 
     /**
      * The resource type.
@@ -30,18 +29,14 @@ public class HelpCenterFaqExporter implements ComponentExporter {
     @SlingObject
     private ResourceResolver resourceResolver;
 
-    @ValueMapValue
-    private String faqLabel;
+    private List<PageInfo> pageInfoList = new ArrayList<>();
 
     @ValueMapValue
     private String readMoreLabel;
 
     @ChildResource
-    private List<Link> faqPages;
+    private List<PageInfo> faqPages;
 
-    public String getFaqLabel() {
-        return faqLabel;
-    }
 
     public String getReadMoreLabel() {
         return readMoreLabel;
@@ -53,19 +48,7 @@ public class HelpCenterFaqExporter implements ComponentExporter {
     }
 
     public List<PageInfo> getFaqPages() {
-        List<PageInfo> pageInfoList = new ArrayList<>();
-        for (Link link : faqPages) {
-            PageInfo pageInfo = new PageInfo();
-            Page page = resourceResolver.getResource(link.getExtensionlessLink()).adaptTo(Page.class);
-            if (page == null) {
-                break;
-            }
-            pageInfo.setLink(link.getExtensionlessLink());
-            pageInfo.setDescription(page.getDescription());
-            pageInfo.setLabel(page.getTitle());
-            pageInfoList.add(pageInfo);
-        }
-        return (pageInfoList == null) ? Collections.emptyList() : pageInfoList;
+        return (faqPages == null) ? Collections.emptyList() : faqPages;
     }
 
 }
