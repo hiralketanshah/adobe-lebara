c515ea160d5d6d44e24210481d5559635d877022
vishal.sah <vishal.sah@lebara.com>
2021-11-16 10:05:48 +0530

    browsecategories changes

---
+++ core/src/main/java/com/lebara/core/models/BrowseCategoryNavigation.java
@@ -0,0 +1,9 @@
+package com.lebara.core.models;
+
+import com.adobe.cq.wcm.core.components.models.Navigation;
+
+public interface BrowseCategoryNavigation extends Navigation {
+
+    public String getBrowseCategoryLabel();
+
+}
