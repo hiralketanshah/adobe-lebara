4a10542de756ae6c208cd87b06a2e6ef1d06e12d
vishal.sah <vishal.sah@lebara.com>
2021-11-16 10:37:53 +0530

    hc faq code

--- core/src/main/java/com/lebara/core/models/Link.java
+++ core/src/main/java/com/lebara/core/models/Link.java
@@ -1,6 +1,7 @@
 package com.lebara.core.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.lebara.core.utils.AemUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -9,6 +10,7 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.inject.Named;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Model(adapters = {Link.class}, adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Link {
 
--- core/src/main/java/com/lebara/core/models/impl/BrowseCategoryNavigationImpl.java
+++ core/src/main/java/com/lebara/core/models/impl/BrowseCategoryNavigationImpl.java
@@ -46,7 +46,6 @@ public class BrowseCategoryNavigationImpl implements BrowseCategoryNavigation {
     }
 
 
-
     @Override
     public String getBrowseCategoryLabel() {
         return browseCategoriesLabel;
