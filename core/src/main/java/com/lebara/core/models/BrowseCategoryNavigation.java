5ba09ea28ab6ec4a8acc7daab77ffb170a9fd8fc
vishal.sah <vishal.sah@lebara.com>
2021-11-16 15:01:32 +0530

    removed browse category

--- core/src/main/java/com/lebara/core/models/BrowseCategoryNavigation.java
+++
@@ -1,9 +0,0 @@
-package com.lebara.core.models;
-
-import com.adobe.cq.wcm.core.components.models.Navigation;
-
-public interface BrowseCategoryNavigation extends Navigation {
-
-    public String getBrowseCategoryLabel();
-
-}
