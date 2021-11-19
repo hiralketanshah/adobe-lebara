a51250fd93b486c0f7e0c5b5117c30550904cc5e
vishal.sah <vishal.sah@lebara.com>
2021-11-19 11:29:29 +0530

    search servlet modification

--- core/src/main/java/com/lebara/core/servlet/SearchServlet.java
+++ core/src/main/java/com/lebara/core/servlet/SearchServlet.java
@@ -7,8 +7,8 @@ import com.day.cq.search.QueryBuilder;
 import com.day.cq.search.result.Hit;
 import com.day.cq.search.result.SearchResult;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.lebara.core.dto.SearchInfo;
+import com.lebara.core.utils.AemUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -39,14 +39,17 @@ import static org.apache.sling.api.servlets.ServletResolverConstants.*;
         service = Servlet.class,
         property = {
                 Constants.SERVICE_DESCRIPTION + "=Global Search Servlet",
-                SLING_SERVLET_METHODS+"="+HttpConstants.METHOD_GET,
-                SLING_SERVLET_RESOURCE_TYPES + "=lebara/components/search",
+                SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_GET,
+                SLING_SERVLET_RESOURCE_TYPES + "=" + SearchServlet.HELP_CENTER_SEARCH_RESOURCETYPE,
+                SLING_SERVLET_RESOURCE_TYPES + "=" + SearchServlet.GLOBAL_SEARCH_RESOURCETYPE,
                 SLING_SERVLET_EXTENSIONS + "=json",
         }
-        )
+)
 public class SearchServlet extends SlingSafeMethodsServlet {
     @Reference
     private QueryBuilder builder;
+    public static final String HELP_CENTER_SEARCH_RESOURCETYPE = "lebara/components/helpcenter/search";
+    public static final String GLOBAL_SEARCH_RESOURCETYPE = "lebara/components/search";
 
     private Session session;
     final Logger LOGGER = LoggerFactory.getLogger(getClass());
@@ -60,38 +63,60 @@ public class SearchServlet extends SlingSafeMethodsServlet {
         session = resourceResolver.adaptTo(Session.class);
 
         String pathInfo = request.getRequestPathInfo().getResourcePath();
+        String resourceType = request.getResource().getResourceType();
         Resource searchResource = request.getResourceResolver().getResource(pathInfo);
-        if(null != searchResource && null != searchResource.getValueMap().get("searchRoot")){
-            String searchRoot = searchResource.getValueMap().get("searchRoot").toString();
-            if(StringUtils.isEmpty(searchRoot)){
-                searchRoot = "/content/lebara/";
-            }
-            Map<String, String> predicate = new HashMap<>();
-            predicate.put("path", searchRoot);
-            predicate.put("type", "cq:Page");
-            if(null != searchType && searchType.equalsIgnoreCase("fulltext")){
-                predicate.put("fulltext", param);
-            } else {
-                predicate.put("1_property", "jcr:content/cq:tags");
-                predicate.put("1_property.value", "%"+param+"%");
-                predicate.put("1_property.operation", "like");
-            }
-           // predicate.put("p.limit", "-1");
-            Gson json = new Gson();
-            Query query = builder.createQuery(PredicateGroup.create(predicate), session);
-            SearchResult searchResult = query.getResult();
-            List<SearchInfo> searchInfoList = new ArrayList<>();
-            for(Hit hit : searchResult.getHits()) {
-                try {
-                    SearchInfo searchInfo = new SearchInfo();
-                    searchInfo.setPath(hit.getPath());
-                    searchInfo.setTitle(hit.getTitle());
-                    searchInfoList.add(searchInfo);
-                } catch (RepositoryException e) {
-                    LOGGER.error("Error in the Search",e);
-                }
-            }
-            response.getWriter().println(json.toJson(searchInfoList));
+        String searchRoot = AemUtils.getStringProperty(searchResource, "searchRoot");
+        if (StringUtils.isEmpty(searchRoot)) {
+            searchRoot = "/content/lebara/";
         }
+        Map<String, String> predicate = new HashMap<>();
+        List<SearchInfo> searchInfoList = new ArrayList<>();
+        if (StringUtils.equalsIgnoreCase(resourceType, HELP_CENTER_SEARCH_RESOURCETYPE)) {
+            predicate = getHelpCenterSearchPredicates(param, searchType, searchRoot);
+        } else if (StringUtils.equalsIgnoreCase(resourceType, GLOBAL_SEARCH_RESOURCETYPE)) {
+            predicate = getGlobalSearchPredicates(param, searchType, searchRoot);
+        }
+        Query query = builder.createQuery(PredicateGroup.create(predicate), session);
+        SearchResult searchResult = query.getResult();
+        for (Hit hit : searchResult.getHits()) {
+            try {
+                SearchInfo searchInfo = new SearchInfo();
+                searchInfo.setPath(hit.getPath());
+                searchInfo.setTitle(hit.getTitle());
+                searchInfoList.add(searchInfo);
+            } catch (RepositoryException e) {
+                LOGGER.error("Error in the Search", e);
+            }
+        }
+        Gson json = new Gson();
+        response.getWriter().println(json.toJson(searchInfoList));
+
     }
+
+
+    private Map<String, String> getGlobalSearchPredicates(String param, String searchType, String searchRoot) {
+        Map<String, String> predicate = new HashMap<>();
+        predicate.put("path", searchRoot);
+        predicate.put("type", "cq:Page");
+        if (null != searchType && searchType.equalsIgnoreCase("fulltext")) {
+            predicate.put("fulltext", param);
+        } else {
+            predicate.put("1_property", "jcr:content/cq:tags");
+            predicate.put("1_property.value", "%" + param + "%");
+            predicate.put("1_property.operation", "like");
+        }
+        return predicate;
+    }
+
+    private Map<String, String> getHelpCenterSearchPredicates(String param, String searchType, String searchRoot) {
+        Map<String, String> predicate = new HashMap<>();
+        predicate.put("path", searchRoot);
+        predicate.put("type", "cq:Page");
+        predicate.put("p.limit", "20");
+        predicate.put("1_property", "jcr:content/jcr:title");
+        predicate.put("1_property.value", "%" + param + "%");
+        predicate.put("1_property.operation", "like");
+        return predicate;
+    }
+
 }
