08855ce47b2f0a350624ae88bd378917f494d606
Jatinder <jatinderbimra@gmail.com>
2021-11-23 09:58:47 +0530

    dependencies fix for ui search implementation

--- ui.frontend/src/redux/types/index.ts
+++ ui.frontend/src/redux/types/index.ts
@@ -6,14 +6,20 @@ import { SelectedProductState } from "../reducers/selectedProductReducer";
 import { UserState } from "../reducers/userReducer";
 import { TopUpsState } from "../reducers/topUpsReducer";
 import { PaymentMethodsState } from "../reducers/paymentMethodsReducer";
+import { HeaderSearchBoxOpenedState } from "../reducers/headerSearchBoxOpened";
+import { LoadingState } from "../reducers/loadingReducer";
+import { FormsState } from "../reducers/formsReducer";
 
 export interface ReduxState {
   cart: CartState;
   phone: SelectedNumberState;
   voucher: SelectedVoucherState;
   highlightedButton: HighlightButtonState;
+  headerSearchBox: HeaderSearchBoxOpenedState;
   product: SelectedProductState;
   user: UserState;
+  loading: LoadingState;
   topUps: TopUpsState;
   paymentMethods: PaymentMethodsState;
+  forms: FormsState;
 }
