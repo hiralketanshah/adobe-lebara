64b52db7f4138c551d535d7b93ef09cc36d9f851
Sundari G <SG00592155@techmahindra.com>
2021-11-23 12:23:26 +0530

    WP-3407[change]review comments fix

--- ui.frontend/src/components/StickyCircle/StickyCircle.tsx
+++ ui.frontend/src/components/StickyCircle/StickyCircle.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, Box, Button, Circle } from "@chakra-ui/react";
 import { StickyCircleProps } from "./types";
 import { useHistory } from "react-router-dom";
+import './styles.css';
 
 const StickyCircle: React.FC<StickyCircleProps> = ({
   linkLabel,
@@ -9,44 +10,37 @@ const StickyCircle: React.FC<StickyCircleProps> = ({
 }) => {
   const history = useHistory();
   return (
-    <>
-     <style>{`
-    .cq-Editable-dom .stickycircle{
-        position: static;
-    }
-  `}</style>
-        <Box
-          position="fixed"
-          display={{ lg: "inline-block", base: "none" }}
-          left={{ lg: "calc(50% + 387px)", base: "50px" }}
-          top="450px"
-          className="stickycircle"
+    <Box
+      position="fixed"
+      display={{ lg: "inline-block", base: "none" }}
+      left={{ lg: "calc(50% + 387px)", base: "50px" }}
+      top="450px"
+      className="stickycircle"
+    >
+      <Circle
+        size="72px"
+        bg="secondary.500"
+        color="white"
+        textAlign="center"
+      >
+        <Button
+          display="flex"
+          flexDirection="column"
+          bg="secondary.500"
+          _hover={{ bg: "secondary.500", background: "none" }}
+          _active={{ bg: "secondary.500", borderColor: "secondary.500", background: "none" }}
+          onClick={() => history.push(linkPath || "/")}
+          _focus={{
+            outline: "none",
+          }}
+          background="none"
         >
-          <Circle
-            size="72px"
-            bg="secondary.500"
-            color="white"
-            textAlign="center"
-          >
-            <Button
-              display="flex"
-              flexDirection="column"
-              bg="secondary.500"
-              _hover={{ bg: "secondary.500", background: "none" }}
-              _active={{ bg: "secondary.500", borderColor: "secondary.500", background: "none" }}
-              onClick={() => history.push(linkPath || "/")}
-              _focus={{
-                outline: "none",
-              }}
-              background="none"
-            >
-              <Text textTransform="capitalize" fontSize="17px" color="white" whiteSpace="break-spaces">
-                {linkLabel}
-              </Text>
-            </Button>
-          </Circle>
-        </Box>
-    </>
+          <Text textTransform="capitalize" fontSize="17px" color="white" whiteSpace="break-spaces">
+            {linkLabel}
+          </Text>
+        </Button>
+      </Circle>
+    </Box>
   );
 }
 
