e5b5a10eb89e828ea9a67350a5635310b98f50e1
vishal.sah <vishal.sah@lebara.com>
2021-11-25 10:04:54 +0530

    support component

---
+++ core/src/main/java/com/lebara/core/models/SupportConstants.java
@@ -0,0 +1,27 @@
+package com.lebara.core.models;
+
+import com.lebara.core.utils.AemUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adapters = {SupportConstants.class}, adaptables = {
+        Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class SupportConstants extends ImageProperties {
+
+    @ValueMapValue
+    private String ctalinklabel;
+
+    @ValueMapValue
+    private String ctalink;
+
+    public String getCtalinklabel() {
+        return ctalinklabel;
+    }
+
+    public String getCtalink() {
+        return AemUtils.getLinkWithExtension(ctalink);
+    }
+
+}
