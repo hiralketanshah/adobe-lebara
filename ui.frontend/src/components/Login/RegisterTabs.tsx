ee2ce60efdd0118a1879bcbcaeeb8a9f15e3b1f0
Santosh Rajendra Bhadrashette <santosh.bhadrashette@gspann.com>
2021-11-29 21:05:29 +0530

    Login Module Refactored

--- ui.frontend/src/components/Login/RegisterTabs.tsx
+++ ui.frontend/src/components/Login/RegisterTabs.tsx
@@ -14,6 +14,7 @@ import { LoginTabsProps } from "./types";
 import LoginTab from "./LoginTab";
 import RegisterTab from "./RegisterTab";
 import Link from "../Link/Link";
+import { globalConfigs as GC, globalConstants as GCST} from "../../GlobalConfigs";
 
 const RegisterTabs: React.FC<LoginTabsProps> = ({ ...loginModuleProps }) => {
   // const cartItems = useSelector((state: ReduxState) => state.cart);
