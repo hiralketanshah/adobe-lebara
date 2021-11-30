2f8ce07a7e47a0193131ade30e0339e850517049
Manigandan Devaraj <devaraj@adobe.com>
2021-11-30 20:30:08 +0530

    Build fix

--- ui.frontend/src/components/RewardOverview/RewardOverview.tsx
+++ ui.frontend/src/components/RewardOverview/RewardOverview.tsx
@@ -9,82 +9,84 @@ const RewardOverview: React.FC<RewardOverviewProps> = ({
   columnHeader2,
   tableItems,
 }) => (
-  {heading && <Box
-    maxWidth={{ base: "405px", md: "none" }}
-    backgroundColor={{ base: "none", md: "grey.50" }}
-    textAlign={{ md: "center" }}
-    padding={{ base: "15px", md: "30px" }}
-  >
-    <Heading
-      as="h6"
-      color="primary.500"
-      fontSize={{ base: "32px" }}
-      marginBottom="10px"
-    >
-      {heading}
-    </Heading>
-    <Text fontSize={{ base: "20px" }} marginBottom="15px">
-      {description}
-    </Text>
-
-    <Box
-      borderTop={{ base: "1px", md: "none" }}
-      borderBottom={{ base: "1px", md: "none" }}
-      borderTopColor={{ base: "lightenPrimary.50" }}
-      borderBottomColor={{ base: "lightenPrimary.50" }}
-      marginBottom="20px"
-      margin={{ md: "auto" }}
-      width={{ md: "70%" }}
-    >
-      <Box
-        borderBottom={{ base: "1px", md: "none" }}
-        borderBottomColor={{ base: "greySuccess" }}
-        display="flex"
-        lineHeight="40px"
-        marginBottom={{ base: "0px", md: "15px" }}
+  <>
+      {heading && <Box
+        maxWidth={{ base: "405px", md: "none" }}
+        backgroundColor={{ base: "none", md: "grey.50" }}
+        textAlign={{ md: "center" }}
+        padding={{ base: "15px", md: "30px" }}
       >
-        <Text
-          width={{ base: "50%", md: "70%" }}
-          borderRight={{ base: "1px", md: "none" }}
-          borderRightColor="greySuccess"
-          mx="20px"
+        <Heading
+          as="h6"
           color="primary.500"
-          textAlign={{ base: "initial", md: "left" }}
-          marginLeft={{ base: "20px", md: "30px" }}
+          fontSize={{ base: "32px" }}
+          marginBottom="10px"
         >
-          {columnHeader1}
-        </Text>
-        <Text color="primary.500">{columnHeader2}</Text>
-      </Box>
+          {heading}
+        </Heading>
+        {description && <Text fontSize={{ base: "20px" }} marginBottom="15px">
+          {description}
+        </Text>}
 
-      {tableItems?.map((tableItem: TableItems, idx) => (
+        <Box
+          borderTop={{ base: "1px", md: "none" }}
+          borderBottom={{ base: "1px", md: "none" }}
+          borderTopColor={{ base: "lightenPrimary.50" }}
+          borderBottomColor={{ base: "lightenPrimary.50" }}
+          marginBottom="20px"
+          margin={{ md: "auto" }}
+          width={{ md: "70%" }}
+        >
           <Box
             borderBottom={{ base: "1px", md: "none" }}
             borderBottomColor={{ base: "greySuccess" }}
             display="flex"
-            backgroundColor={{ md: "white" }}
-            marginBottom={{ base: "0px", md: "15px" }}
             lineHeight="40px"
-            padding={{ base: "0", md: "12px" }}
-            borderRadius={{ base: "0", md: "12px" }}
-            textAlign={{ base: "initial", md: "left" }}
-            fontSize={{ base: "14px", md: "20px" }}
+            marginBottom={{ base: "0px", md: "15px" }}
           >
             <Text
               width={{ base: "50%", md: "70%" }}
               borderRight={{ base: "1px", md: "none" }}
               borderRightColor="greySuccess"
               mx="20px"
+              color="primary.500"
+              textAlign={{ base: "initial", md: "left" }}
+              marginLeft={{ base: "20px", md: "30px" }}
             >
-              {tableItem.label1}
+              {columnHeader1}
             </Text>
-            <Text fontWeight="bold">{tableItem.label2}</Text>
+            <Text color="primary.500">{columnHeader2}</Text>
           </Box>
-      ))}
 
-    </Box>
-  </Box>
-  }
+          {tableItems?.map((tableItem: TableItems, idx) => (
+              <Box
+                borderBottom={{ base: "1px", md: "none" }}
+                borderBottomColor={{ base: "greySuccess" }}
+                display="flex"
+                backgroundColor={{ md: "white" }}
+                marginBottom={{ base: "0px", md: "15px" }}
+                lineHeight="40px"
+                padding={{ base: "0", md: "12px" }}
+                borderRadius={{ base: "0", md: "12px" }}
+                textAlign={{ base: "initial", md: "left" }}
+                fontSize={{ base: "14px", md: "20px" }}
+              >
+                <Text
+                  width={{ base: "50%", md: "70%" }}
+                  borderRight={{ base: "1px", md: "none" }}
+                  borderRightColor="greySuccess"
+                  mx="20px"
+                >
+                  {tableItem.label1}
+                </Text>
+                <Text fontWeight="bold">{tableItem.label2}</Text>
+              </Box>
+          ))}
+
+        </Box>
+      </Box>
+      }
+  </>
 );
 
 export default RewardOverview;
