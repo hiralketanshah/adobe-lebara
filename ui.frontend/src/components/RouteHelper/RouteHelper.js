2c63b71121ed6b500576c33a57449b6c74ef4938
Sundari G <SG00592155@techmahindra.com>
2021-11-30 17:55:43 +0530

    WP[fix]personal detail payment button & url shortening code revert

--- ui.frontend/src/components/RouteHelper/RouteHelper.js
+++ ui.frontend/src/components/RouteHelper/RouteHelper.js
@@ -16,8 +16,6 @@
 
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import {AuthoringUtils} from "@adobe/aem-spa-page-model-manager";
-import { PROJECT_URL_ROOT } from "../../utils/lebara.constants";
 
 /**
  * Helper that facilitate the use of the {@link Route} component
@@ -39,12 +37,6 @@ export const withRoute = (WrappedComponent, extension) => {
       }
 
       extension = extension || 'html';
-
-      let paths = ['(.*)' + routePath + '(.' + extension + ')?'];
-       
-      if (!AuthoringUtils.isInEditor() && routePath.startsWith(PROJECT_URL_ROOT)) {
-        paths.push(routePath.substring(PROJECT_URL_ROOT.length) + ".html");
-      }
 
       // Context path + route path + extension
       return (
