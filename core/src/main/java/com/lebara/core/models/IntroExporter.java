362250b9205b908db78b74acf306fe32cdfe2749
vishal.sah <vishal.sah@lebara.com>
2021-12-01 11:14:53 +0530

    commit for call support and email support

--- core/src/main/java/com/lebara/core/models/IntroExporter.java
+++ core/src/main/java/com/lebara/core/models/IntroExporter.java
@@ -2,12 +2,14 @@ package com.lebara.core.models;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {IntroExporter.class,
         ComponentExporter.class}, resourceType = IntroExporter.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
