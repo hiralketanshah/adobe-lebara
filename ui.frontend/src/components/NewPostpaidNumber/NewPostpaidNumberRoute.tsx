65c15699a1e6dd97d7a4bf884aedaaa6ba885727
Sundari G <SG00592155@techmahindra.com>
2021-12-06 13:01:47 +0530

    WP-3038[change]postpaid alignment fix

--- ui.frontend/src/components/NewPostpaidNumber/NewPostpaidNumberRoute.tsx
+++ ui.frontend/src/components/NewPostpaidNumber/NewPostpaidNumberRoute.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import NewPostpaidNumber from "./NewPostpaidNumber";
 import NewPostpaidNumberLayout from "../../layouts/NewPostpaidNumberLayout";
+import { NewPostPaidNumberProps } from "./types";
 
-const NewPostpaidNumberRoute: React.FC = () => (
+const NewPostpaidNumberRoute: React.FC<NewPostPaidNumberProps> = ({...props}) => (
   <NewPostpaidNumberLayout>
     <Box width="100%" display={{ md: "flex" }} justifyContent="center">
-      <NewPostpaidNumber />
+      <NewPostpaidNumber {...props}/>
     </Box>
   </NewPostpaidNumberLayout>
 );
