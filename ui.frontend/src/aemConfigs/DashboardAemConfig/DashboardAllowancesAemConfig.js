786b6992d8be7ec97df5393fe30a4f04c335acf5
Sundari G <SG00592155@techmahindra.com>
2021-12-14 19:53:30 +0530

    WP[fix]plans fix

--- ui.frontend/src/aemConfigs/DashboardAemConfig/DashboardAllowancesAemConfig.js
+++ ui.frontend/src/aemConfigs/DashboardAemConfig/DashboardAllowancesAemConfig.js
@@ -1,10 +1,21 @@
-import {MapTo} from "@adobe/aem-react-editable-components";
+import { MapTo } from "@adobe/aem-react-editable-components";
 import DashboardAllowances from "@lebara/ui/src/components/DashboardAllowances/DashboardAllowances"
-
+import { Flex } from "@chakra-ui/react";
 const DashboardAllowancesConfig = {
   emptyLabel: "Dashboard Allowances component",
   isEmpty: function (props) {
     return true;
   },
 };
-MapTo("lebara/components/dashboard/dashboardallowances")(DashboardAllowances, DashboardAllowancesConfig);
+const wrappedComponent = (props) =>
+  <Flex flexDirection="column" alignItems="center">
+    <Flex
+      w={{ base: "100%", lg: "846px" }}
+      flexDirection="column"
+      px={{ base: "20px", lg: 0 }}
+      gridGap={{ base: "17px", lg: "20px" }}
+      pt={{ base: "17px", lg: "20px" }}
+    ><DashboardAllowances {...props} />
+    </Flex>
+  </Flex>;
+MapTo("lebara/components/dashboard/dashboardallowances")(wrappedComponent, DashboardAllowancesConfig);
