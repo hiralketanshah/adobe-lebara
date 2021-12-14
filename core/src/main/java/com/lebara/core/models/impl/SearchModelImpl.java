99fc3327f3d6b971d315811a57c4764edf5345a7
Nagarjun <npasumar@adobe.com>
2021-12-14 14:37:48 +0530

    Adding search as part of Header

--- core/src/main/java/com/lebara/core/models/impl/SearchModelImpl.java
+++ core/src/main/java/com/lebara/core/models/impl/SearchModelImpl.java
@@ -1,29 +1,22 @@
 package com.lebara.core.models.impl;
 
-import com.adobe.cq.export.json.ComponentExporter;
-import com.adobe.cq.export.json.ExporterConstants;
-import com.adobe.cq.wcm.core.components.models.Search;
-import com.lebara.core.models.SearchModel;
 import com.lebara.core.models.beans.Link;
-import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
-import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.models.annotations.via.ResourceSuperType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Collections;
 import java.util.List;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = { SearchModelImpl.class,
-		ComponentExporter.class }, resourceType = SearchModelImpl.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class SearchModelImpl implements SearchModel {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class SearchModelImpl {
+
+	@ValueMapValue
+	private String searchRoot;
 
 	@ValueMapValue
 	private String searchPlaceholder;
@@ -36,22 +29,10 @@ public class SearchModelImpl implements SearchModel {
 
 	@ChildResource
 	private List<Link> links;
+	
 
-	@ScriptVariable
-	protected Resource resource;
-
-	/**
-	 * The resource type.
-	 */
-	protected static final String RESOURCE_TYPE = "lebara/components/search";
-
-	@Self
-	@Via(type = ResourceSuperType.class)
-	private Search delegate;
-
-	@Override
-	public String getSearchRootPagePath() {
-		return delegate.getSearchRootPagePath();
+	public String getSearchRoot() {
+		return searchRoot;
 	}
 
 	public String getSearchPlaceholder() {
@@ -69,10 +50,4 @@ public class SearchModelImpl implements SearchModel {
 	public String getEmptySearchResultMsg() {
 		return emptySearchResultMsg;
 	}
-
-	@Override
-	public String getExportedType() {
-		return RESOURCE_TYPE;
-	}
-
 }
