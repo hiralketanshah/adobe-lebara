47eceba7e44b5122348c49cca3ac58bb8b98ff65
Sundari G <SG00592155@techmahindra.com>
2021-12-15 18:00:32 +0530

    WP[new]private routing

--- ui.frontend/src/utils/aem-utils.js
+++ ui.frontend/src/utils/aem-utils.js
@@ -1,4 +1,4 @@
-import { globalConstants as C } from "@lebara/ui/src/configs/globalConfigs.js";
+import { globalConfigs as GC, globalConstants as C } from "@lebara/ui/src/configs/globalConfigs.js";
 
 const aemUtils = {
   getSearchResultsPath: function(query = '', searchRootPagePath = undefined, type = "") {
@@ -21,6 +21,9 @@ const aemUtils = {
     const response = await fetch(aemUtils.getCfOfferDataUrl(offerId));
     const json = await response.json();
     return isOneEntry ? json[0] : json;
+  },
+  isForbiddenPage: function (pathname) {
+    return pathname === (GC.journeyPages[C.DASHBOARD_MANAGE] || GC.journeyPages[C.DASHBOARD] || GC.journeyPages[C.USAGE_DETAILS] || GC.journeyPages[C.MANAGE_SIM_CARDS] || GC.journeyPages[C.PAYMENT_METHOD] || GC.journeyPages[C.ADD_EDIT_DEBIT_CREDIT_CARD] || GC.journeyPages[C.ADD_PAYPAL] || GC.journeyPages[C.USER_PROFILE] || GC.journeyPages[C.CONFIRM_DELINK_SIM]);
   },
   debounce: function (func, timeout = 300) {
     let timer;
