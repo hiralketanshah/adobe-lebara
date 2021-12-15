7d8aaa1680a4bf13a0e539494d1daf873adff574
Sundari G <SG00592155@techmahindra.com>
2021-12-15 22:28:59 +0530

    WP[fix]editor condition

--- ui.frontend/src/PrivateRoute.tsx
+++ ui.frontend/src/PrivateRoute.tsx
@@ -7,12 +7,13 @@ import { selectIsLoading } from "@lebara/ui/src/redux/selectors/loadingSelectors
 import { useLocation } from 'react-router-dom';
 import aemUtils from "./utils/aem-utils";
 import { globalConfigs as GC, globalConstants as C } from "@lebara/ui/src/configs/globalConfigs.js";
+import {AuthoringUtils} from "@adobe/aem-spa-page-model-manager";
 function PrivateRoute({ WrappedComponent, routeProps, ...rest }: any) {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const isLoading = useSelector(selectIsLoading);
   const location = useLocation();
   return (
-    (isAuthenticated || isLoading) || !aemUtils.isForbiddenPage(location.pathname) ?
+    (isAuthenticated || isLoading) || AuthoringUtils.isInEditor() || !aemUtils.isForbiddenPage(location.pathname) ?
       <WrappedComponent {...rest} {...routeProps} /> :
       <Route
         {...rest}
