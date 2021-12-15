f3fdaedbf782f2a10743850602a44379e4853163
Jatinder <jatinderbimra@gmail.com>
2021-12-15 18:23:41 +0530

    fixed warning messages and iframe outer test border removed

--- ui.frontend/src/components/Iframe/Iframe.tsx
+++ ui.frontend/src/components/Iframe/Iframe.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { IframeProps } from "./types";
 
+const IframeComp = ({url, width, height}: any) => {
+    return(
+      <div style={{border: '0'}}>
+        <iframe src={url || "/"}
+          width={width || '100%'}
+          height={height || '100%'}
+          style={{border: 'none', height: '650px', background: 'none'}}
+          title=""
+          />
+      </div>
+    )
+};
+
 const Iframe: React.FC<IframeProps> = ({
   url,
   height,
   width,
 }) => {
-
-  return (
-         <>
-             {url !== '' && (
-                //@ts-ignore
-                <iframe src={url} height={height || 250} width={width || 250} frameborder="0"></iframe>
-             )}
-         </>
-     );
+  if(!url || url === "") return null;
+  return (<IframeComp url={url} width={width} height={height} />);
 };
+
 export default Iframe;
