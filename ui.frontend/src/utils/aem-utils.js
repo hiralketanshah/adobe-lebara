c5cb01bfe358c860f81fe2e512ae2895aabaea94
Jatinder <jatinderbimra@gmail.com>
2021-12-16 12:20:09 +0530

    major/integration-issues-fixes-aem

--- ui.frontend/src/utils/aem-utils.js
+++ ui.frontend/src/utils/aem-utils.js
@@ -22,7 +22,7 @@ const aemUtils = {
     const json = await response.json();
     return isOneEntry ? json[0] : json;
   },
-  debounce: function (func, timeout = 300) {
+  debounce: function (func, timeout = 1000) {
     let timer;
     return (...args) => {
       clearTimeout(timer);
