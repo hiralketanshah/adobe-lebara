63906423f0663192eb2f637d171edeb64a7d0c3a
Santosh Rajendra Bhadrashette <santosh.bhadrashette@gspann.com>
2021-12-29 22:09:47 +0530

    Junit Test Classes

---
+++ core/src/test/java/com/lebara/core/models/VerifyRegisterMobileExporterTest.java
@@ -0,0 +1,36 @@
+package com.lebara.core.models;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
+class VerifyRegisterMobileExporterTest {
+
+    private final AemContext aemContext = new AemContext();
+    private static final String PROPERTIES_JSON = "/content/lebara/de/de/verify-register/jcr:content/root/responsivegrid/verifymobile";
+
+    @BeforeEach
+    void setUp() {
+        aemContext.addModelsForClasses(SupportExporter.class);
+        aemContext.load().json("/verify-register.json", "/content/lebara/de/de/verify-register");
+    }
+
+    @Test
+    void getHeadingTest() {
+        aemContext.currentResource(PROPERTIES_JSON);
+        VerifyRegisterMobileExporter verifyRegisterMobileExporter = aemContext.request().adaptTo(VerifyRegisterMobileExporter.class);
+        verifyRegisterMobileExporter.getHeading();
+        verifyRegisterMobileExporter.getFrmFields();
+        verifyRegisterMobileExporter.getValidationMessages();
+        verifyRegisterMobileExporter.getTimeCounter();
+        verifyRegisterMobileExporter.getSuccessMessages();
+        verifyRegisterMobileExporter.getInitalCountdownValue();
+        verifyRegisterMobileExporter.getExportedType();
+
+    }
+}
