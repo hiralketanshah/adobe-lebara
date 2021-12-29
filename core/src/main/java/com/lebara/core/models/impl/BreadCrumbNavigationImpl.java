6f953f1619c4a4a27b6af6c882c34ce33f123f7a
vishal.sah <vishal.sah@lebara.com>
2021-12-29 21:02:48 +0530

    added code

--- core/src/main/java/com/lebara/core/models/impl/BreadCrumbNavigationImpl.java
+++ core/src/main/java/com/lebara/core/models/impl/BreadCrumbNavigationImpl.java
@@ -3,11 +3,15 @@ package com.lebara.core.models.impl;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.models.NavigationItem;
+import com.day.cq.wcm.api.Page;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.adobe.cq.wcm.core.components.models.Breadcrumb;
 import com.lebara.core.models.beans.ListItem;
+import com.lebara.core.utils.AemUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
@@ -31,8 +35,8 @@ public class BreadCrumbNavigationImpl implements Breadcrumb {
     @SlingObject
     private SlingHttpServletRequest request;
 
-    @ScriptVariable
-    protected Resource resource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
 
 
     @Self
@@ -50,8 +54,24 @@ public class BreadCrumbNavigationImpl implements Breadcrumb {
         List<ListItem> breadCrumbList = new ArrayList<>();
         for (NavigationItem items : delegate.getItems()) {
             ListItem listItem = new ListItem();
+            String url = items.getURL();
             listItem.setTitle(items.getTitle());
-            listItem.setUrl(items.getURL());
+            listItem.setUrl(AemUtils.getLinkWithExtension(url, request));
+            if (StringUtils.isBlank(url)) {
+                continue;
+            }
+            Resource pageRes = resourceResolver.getResource(url.replaceAll(".html", ""));
+            if (pageRes == null) {
+                continue;
+            }
+            Page page = pageRes.adaptTo(Page.class);
+            if (page == null) {
+                continue;
+            }
+            String breadCrumbTitle = AemUtils.getStringProperty(page.getContentResource(), "breadcrumbTitle");
+            if (StringUtils.isNotBlank(breadCrumbTitle)) {
+                listItem.setTitle(breadCrumbTitle);
+            }
             breadCrumbList.add(listItem);
         }
         return breadCrumbList;
