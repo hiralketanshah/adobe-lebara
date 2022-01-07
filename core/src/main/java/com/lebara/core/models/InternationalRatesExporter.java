103b1a92f9f66ee738661d6e2ea256d026ed38f5
Santosh Rajendra Bhadrashette <santosh.bhadrashette@gspann.com>
2022-01-07 17:43:34 +0530

    PR review changes

--- core/src/main/java/com/lebara/core/models/InternationalRatesExporter.java
+++ core/src/main/java/com/lebara/core/models/InternationalRatesExporter.java
@@ -52,8 +52,8 @@ public class InternationalRatesExporter implements ComponentExporter {
     @ValueMapValue
     private String cfPath;
 
-    @ValueMapValue
     private String selectCountryLabel;
+
     private String countryLabel;
 
     @ValueMapValue
