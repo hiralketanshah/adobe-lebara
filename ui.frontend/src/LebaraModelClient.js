18e42e64efe048bf71ecf2161dacd496f1605033
Jatinder <jatinderbimra@gmail.com>
2022-01-18 13:56:41 +0530

    fix for loader on model json call

--- ui.frontend/src/LebaraModelClient.js
+++ ui.frontend/src/LebaraModelClient.js
@@ -1,19 +1,26 @@
 import { ModelClient } from '@adobe/aem-spa-page-model-manager';
-import { setLoading } from "@lebara/ui/src/redux/actions/loadingActions";
-import { useDispatch } from "react-redux";
 
 /**
  * Used to show the loader image before any model call.
  */
+
+ const renderLoader = (isLoading) => {
+    if(isLoading) {
+        if(document.querySelector("body #modelRootLoader")) {
+            document.querySelector("body #modelRootLoader").style.display = "block";
+        }
+    } else {
+        if(document.querySelector("body #modelRootLoader")) {
+            document.querySelector("body #modelRootLoader").style.display = "none";
+        }
+    }
+  };
+  
 export default class LebaraModelClient extends ModelClient {
 
     fetch(modelPath) {
-        console.log("LebaramodelClient");
-       //const dispatch = useDispatch();// eslint-disable-line react-hooks/exhaustive-deps
-
         if (!modelPath) {
             const err = `Fetching model rejected for path: ${modelPath}`;
-
             return Promise.reject(new Error(err));
         }
 
@@ -21,21 +28,17 @@ export default class LebaraModelClient extends ModelClient {
         const apihostPrefix = this._apiHost || '';
         const url = `${apihostPrefix}${modelPath}`;
 
-        //dispatch(setLoading(true));
-        console.log("Loading image here");
+        renderLoader(true);
+
         return fetch(url, { credentials: 'same-origin' }).then((response) => {
-            //dispatch(setLoading(false));
-            console.log("Hide Loading image in success");
             if ((response.status >= 200) && (response.status < 300)) {
                 return response.json();
             }
-
             throw { response };
         }).catch((error) => {
-            //dispatch(setLoading(false));
-            console.log("Hide Loading image in failure");
             return Promise.reject(error);
+        }).finally(() => {
+            renderLoader(false);
         });
-
     }
 }
