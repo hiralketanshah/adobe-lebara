5770c92962f04dbae5509dbb1f7505b571d32767
karthikkaliappan <Karthikeyan.Kaliappan@lebara.com>
2022-03-18 10:36:47 +0000

    DEWP-2789 - PP ACtivate sim only

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -52,6 +52,7 @@ import "../aemConfigs/UserProfileAemConfig.js";
 import "../aemConfigs/AddressChangeOtpAemConfig.js";
 import "../aemConfigs/DashboardAemConfig/AttachSimModelAemConfig.js";
 import "../aemConfigs/ConfirmDelinkSimAemConfig.js";
+import "../aemConfigs/ActivateSimOnlyAemConfig.js";
 import "../rotues/AemConfig.js";
 
 import "./Teaser/AemConfig.js";
