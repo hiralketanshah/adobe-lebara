39e580607873631c2360459218095e0f9ced77e9
GBR\Vishal.Sah <vishal.sah@lebara.com>
2022-04-25 15:23:02 +0530

    header refactored

--- core/src/main/java/com/lebara/core/models/impl/HeaderNavigationImpl.java
+++ core/src/main/java/com/lebara/core/models/impl/HeaderNavigationImpl.java
@@ -2,8 +2,6 @@ package com.lebara.core.models.impl;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
-import com.adobe.cq.wcm.core.components.models.Navigation;
-import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lebara.core.models.CustomNavigationItem;
 import com.lebara.core.models.HeaderNavigation;
@@ -13,23 +11,20 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.models.annotations.via.ResourceSuperType;
 import com.lebara.core.models.beans.Link;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = {HeaderNavigation.class, ComponentExporter.class},
         resourceType = HeaderNavigationImpl.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
-public class HeaderNavigationImpl implements HeaderNavigation {
+public class HeaderNavigationImpl implements ComponentExporter {
 
     @SlingObject
     private SlingHttpServletRequest request;
@@ -67,9 +62,6 @@ public class HeaderNavigationImpl implements HeaderNavigation {
     @ChildResource
     private SearchModelImpl search;
 
-    @Self
-    @Via(type = ResourceSuperType.class)
-    private Navigation delegate;
 
     /**
      * The resource type.
@@ -77,41 +69,22 @@ public class HeaderNavigationImpl implements HeaderNavigation {
     protected static final String RESOURCE_TYPE = "lebara/components/header/headernavigation";
 
     @Override
-    public String getAccessibilityLabel() {
-        return delegate.getAccessibilityLabel();
-    }
-
-    @Override
-    public String getId() {
-        return delegate.getId();
-    }
-
-    @Override
-    public ComponentData getData() {
-        return delegate.getData();
-    }
-
-    @Override
     public String getExportedType() {
         return resource.getResourceType();
     }
 
-    @Override
     public String getLogoPath() {
         return fileReference;
     }
 
-    @Override
     public String getTopupCtaText() {
         return topupCtaText;
     }
 
-    @Override
     public String getTopupCtaLink() {
         return AemUtils.getLinkWithExtension(topupCtaLink, request);
     }
 
-    @Override
     public String getLogoLinkURL() {
         return AemUtils.getLinkWithExtension(logoLinkURL, request);
     }
@@ -120,7 +93,6 @@ public class HeaderNavigationImpl implements HeaderNavigation {
         return AemUtils.getLinkWithExtension(accountLink, request);
     }
 
-    @Override
     public String getNewText(){
         return newText;
     }
@@ -131,9 +103,7 @@ public class HeaderNavigationImpl implements HeaderNavigation {
      */
     @JsonProperty("items")
     public List<CustomNavigationItem> getCustomItems() {
-        return delegate.getItems().stream()
-                .map(navItem -> new CustomNavigationItem(resource, navItem, request))
-                .collect(Collectors.toList());
+        return new ArrayList<>();
     }
 
     @JsonProperty("loggedInMenuItems")
