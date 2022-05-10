be3c4bb0b80af84600e95afea950d295a7c4750b
GBR\Vishal.Sah <vishal.sah@lebara.com>
2022-05-10 15:22:42 +0530

    config correction

--- ui.frontend/src/rotues/AemConfig.js
+++ ui.frontend/src/rotues/AemConfig.js
@@ -8,6 +8,12 @@ const SimPortInConfig = {
     return !props.title || !props.pretitle;
   },
 };
+const FRSimPortInConfig = {
+  emptyLabel: " FR Sim Port In Component",
+  isEmpty: function (props) {
+    return !props.title || !props.pretitle;
+  },
+};
 const OrderDetailsConfig = {
   emptyLabel: "Order Details Component",
   isEmpty: function (props) {
@@ -16,5 +22,6 @@ const OrderDetailsConfig = {
 };
 const componentwithofferDataProp=(props)=> <OrderDetailsRoute {...props} fetchDataCallback={(id, isOneEntry, isPromotion)=> aemUtils.fetchData(id, isOneEntry, isPromotion)}/>;
 MapTo("lebara/components/simportin")(SimPortInRoute, SimPortInConfig);
-MapTo("lebara/components/frsimportin")(SimPortInRoute, SimPortInConfig);
+const componentwithProp=(props)=> <SimPortInRoute {...props} showRIOCodeForm />
+MapTo("lebara/components/frsimportin")(componentwithProp, FRSimPortInConfig);
 MapTo("lebara/components/orderdetails")(componentwithofferDataProp, OrderDetailsConfig);
