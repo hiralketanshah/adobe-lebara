a2d0d3562955eb09331c996a1103176118a67ebd
TECHMAHINDRA\SG00592155 <SG00592155@TechMahindra.com>
2022-07-27 23:07:04 +0530

    AEM-231[change]chat optimisation

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -65,24 +65,33 @@ module.exports = {
 };
 
 module.exports = {
-  context: BUILD_DIR,
+ // context: BUILD_DIR,
   clientLibRoot: CLIENTLIB_DIR,
   libs: {
     name: 'clientlib-chat',
     allowProxy: true,
     categories: ['lebara.chat'],
-    //serializationFormat: 'xml',
-    cssProcessor: ['default:none', 'min:none'],
-    jsProcessor: ['default:none', 'min:none'],
-    // assets: {
-    //   js: [
-    //     {src:"./packages/lebara-client/public/scripts/microsoftOmniChat.js",
-    //     dest: "microsoftOmniChat.js"}
-    //   ],
-    //   css: [
-    //    {src: "./packages/lebara-client/public/styles/chatWidget.css",
-    //     dest: "chatWidget.css"}
-    //   ]
-    // }
+    serializationFormat: 'xml',
+    jsProcessor: ["default:none", "min:gcc;compilationLevel=whitespace"],
+    assets: {
+      js: {
+        base: "js",
+        files: [
+          {
+            src: "packages/lebara-client/public/scripts/microsoftOmniChat.js",
+            dest: "microsoftOmniChat.js",
+          },
+        ],
+      },
+      css: {
+        base: "css",
+        files: [
+          {
+            src: "packages/lebara-client/public/styles/chatWidget.css",
+            dest: "chatWidget.css",
+          },
+        ],
+      },
   }
+}
 };
