0a21275c7b9d0bf95793cf177e920b259165a156
Moaead Yahya <moaead.yahya-ext@lebara.com>
2022-08-18 14:57:41 +0300

    Revert "Setup workspaces test"
    This reverts commit af0959e0cd949a92a3fbbc21fd6f0c1ca296b083.

--- ui.frontend/craco.config.js
+++ ui.frontend/craco.config.js
@@ -1,34 +1,26 @@
 const path = require('path')
-const {addBeforeLoader} = require("@craco/craco");
-const {loaderByName} = require("@craco/craco");
-const {getLoaders} = require("@craco/craco");
-const libraries = [path.resolve(__dirname, "./src"), path.resolve(__dirname, "./packages/lebara-client/src"), path.resolve(__dirname, "./packages/lebara-client/packages/core/src"), ];
+const fs = require('fs')
+const cracoBabelLoader = require('craco-babel-loader')
+
+// manage relative paths to packages
+const appDirectory = fs.realpathSync(process.cwd())
+const resolvePackage = relativePath => path.resolve(appDirectory, relativePath)
 
 module.exports = {
     webpack: {
         alias: {
             '@components': path.resolve(__dirname, "packages/lebara-client/src/components/"),
-            'rotues': path.resolve(__dirname, "packages/lebara-client/src/rotues/"),
             '@hooks': path.resolve(__dirname, "packages/lebara-client/src/hooks/"),
-            '@lebara/core': path.resolve(__dirname, "packages/lebara-client/packages/core/src/"),
-            '@lebara/germany': path.resolve(__dirname, "packages/lebara-client/packages/germany/src/"),
-        },
-        configure: webpackConfig => {
-            console.log("configureconfigureconfigureconfigureconfigureconfigureconfigure");
-
-            const scopePlugin = webpackConfig.resolve.plugins.find(
-                ({constructor}) => constructor && constructor.name === 'ModuleScopePlugin'
-            );
-            const {matches} = getLoaders(webpackConfig, loaderByName('babel-loader'));
-            console.log(matches[0]);
-            addBeforeLoader(webpackConfig, loaderByName('babel-loader'), {
-                ...matches[0].loader,
-                include: [...libraries],
-            });
-            console.log(scopePlugin.appSrcs);
-            scopePlugin.appSrcs = [...scopePlugin.appSrcs, ...libraries]
-            return webpackConfig;
         }
-
-    }
+    },
+    plugins: [
+        {
+            plugin: cracoBabelLoader,
+            options: {
+                includes: [
+                    resolvePackage('./packages/lebara-client'),
+                ],
+            },
+        },
+    ],
 }
