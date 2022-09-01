54a53f36c3e9375ea852f41b7c2b697156493cfb
Samarth Pawar <sdilippawar@adobe.com>
2022-09-01 20:08:15 +0530

    Revert "Language Icon Authoring Added"
    This reverts commit e1fb7bfcbf88f7bbf26c87aa21fd50e034b8930f.

--- core/src/main/java/com/lebara/core/models/beans/LanguageNavListItem.java
+++ core/src/main/java/com/lebara/core/models/beans/LanguageNavListItem.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LanguageNavListItem extends ListItem {
     private boolean active;
-    private String languageIcon;
 
     public boolean isActive() {
         return active;
@@ -14,12 +13,4 @@ public class LanguageNavListItem extends ListItem {
     public void setActive(boolean active) {
         this.active = active;
     }
-
-    public String getLanguageIcon() {
-        return languageIcon;
-    }
-
-    public void setLanguageIcon(String languageIcon) {
-        this.languageIcon = languageIcon;
-    }    
 }
--- core/src/main/java/com/lebara/core/models/impl/LanguageHeaderNavigationImpl.java
+++ core/src/main/java/com/lebara/core/models/impl/LanguageHeaderNavigationImpl.java
@@ -5,18 +5,12 @@ import com.adobe.cq.export.json.ExporterConstants;
 import com.adobe.cq.wcm.core.components.models.LanguageNavigation;
 import com.adobe.cq.wcm.core.components.models.NavigationItem;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
-import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
-import com.day.cq.commons.inherit.InheritanceValueMap;
-import com.day.cq.wcm.api.Page;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lebara.core.models.LanguageHeaderNavigation;
 import com.lebara.core.models.beans.LanguageNavListItem;
 import com.lebara.core.utils.AemUtils;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
@@ -30,16 +24,16 @@ import org.apache.sling.models.annotations.via.ResourceSuperType;
 import java.util.ArrayList;
 import java.util.List;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = { LanguageHeaderNavigation.class,
-        ComponentExporter.class }, resourceType = LanguageHeaderNavigationImpl.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(
+    adaptables = SlingHttpServletRequest.class,
+    adapters = {LanguageHeaderNavigation.class, ComponentExporter.class},
+    resourceType = LanguageHeaderNavigationImpl.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class LanguageHeaderNavigationImpl implements LanguageHeaderNavigation {
 
     @SlingObject
     private SlingHttpServletRequest request;
-
-    @SlingObject
-    private ResourceResolver resourceResolver;
 
     @ScriptVariable
     protected Resource resource;
@@ -73,11 +67,11 @@ public class LanguageHeaderNavigationImpl implements LanguageHeaderNavigation {
             languageNavListItem.setActive(items.isActive());
             languageNavListItem.setTitle(items.getTitle());
             languageNavListItem.setUrl(AemUtils.getLinkWithExtension(items.getURL(), request));
-            languageNavListItem.setLanguageIcon(getLanguageIcon(items.getPath()));
             langNavList.add(languageNavListItem);
         }
         return langNavList;
     }
+
 
     @Override
     public String getId() {
@@ -112,22 +106,5 @@ public class LanguageHeaderNavigationImpl implements LanguageHeaderNavigation {
     @Override
     public String getHelpTitle() {
         return helpTitle;
-    }
-
-    private String getLanguageIcon(String pagePath) {
-        if (resourceResolver == null) {
-            return StringUtils.EMPTY;
-        }
-        Resource pageRes = resourceResolver.getResource(pagePath);
-        if (pageRes == null) {
-            return StringUtils.EMPTY;
-        }
-        Page page = pageRes.adaptTo(Page.class);
-        if (page == null) {
-            return StringUtils.EMPTY;
-        }
-
-        InheritanceValueMap inheritedProp = new HierarchyNodeInheritanceValueMap(page.getContentResource());
-        return AemUtils.getInheritedValue("languageIcon", inheritedProp);
     }
 }
