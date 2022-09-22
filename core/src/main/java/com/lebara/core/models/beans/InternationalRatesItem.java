9ba159a8702ff9f31751938b35ac54a7cf2496fb
Hiral Vasa <hishah@adobe.com>
2022-09-22 17:26:22 +0530

    Initial commit

---
+++ core/src/main/java/com/lebara/core/models/beans/InternationalRatesItem.java
@@ -0,0 +1,56 @@
+package com.lebara.core.models.beans;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = { Resource.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class InternationalRatesItem {
+
+	@ValueMapValue
+	private String contractTitle;
+
+	@ValueMapValue
+	private String landlineCallRate;
+
+	@ValueMapValue
+	private String mobileCallRate;
+
+	@ValueMapValue
+	private String smsRate;
+
+	@ValueMapValue
+	private String message;
+
+	private int value;
+
+	public String getContractTitle() {
+		return contractTitle;
+	}
+
+	public String getLandlineCallRate() {
+		return landlineCallRate;
+	}
+
+	public String getMobileCallRate() {
+		return mobileCallRate;
+	}
+
+	public String getSmsRate() {
+		return smsRate;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
