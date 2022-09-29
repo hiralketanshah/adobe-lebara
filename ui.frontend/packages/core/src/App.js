39c55b918d7cd7b67365c31f316b5e928d10c410
Samarth Pawar <sdilippawar@adobe.com>
2022-09-29 17:13:53 +0530

    Temp Fix: Store Updated for Core -fetching from NL

--- ui.frontend/packages/core/src/App.js
+++ ui.frontend/packages/core/src/App.js
@@ -21,7 +21,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
-import store from "@lebara/core/store";
+import store from "@lebara/netherlands/redux/store";
 import Fonts from "./Fonts";
 import GoogleAnalytics from "@lebara/core/GoogleAnalytics";
 const theme = extendTheme(
