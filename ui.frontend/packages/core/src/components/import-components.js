8feece706e185bc61ea6b44f9a9d0e92dd2b1918
K <santhosh-kumar.k@capgemini.com>
2022-09-30 11:37:38 +0530

    NLWP-341 Dashboard Banner Component

--- ui.frontend/packages/core/src/components/import-components.js
+++ ui.frontend/packages/core/src/components/import-components.js
@@ -99,5 +99,6 @@ import "../aemConfigs/OurPromotionsAemConfig.js";
 import "../aemConfigs/RecentPostsAemConfig.js";
 import "../aemConfigs/QuickActionConfig.js";
 import "../aemConfigs/DestinationTableAemConfig.js";
+import "../aemConfigs/DashboardBannerAemConfig.js";
 import "../aemConfigs/SuccessPopupAemConfig.js";
 import "../aemConfigs/InformativeTableAemConfig.js";
