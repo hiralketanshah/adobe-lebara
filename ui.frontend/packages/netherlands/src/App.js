babef175b8dea5d212fb38ee605f3401e7bacbf5
SundariGunaseelan <Sundari.G-EXT@lebara.com>
2022-10-04 20:49:59 +0530

    store changes

--- ui.frontend/packages/netherlands/src/App.js
+++ ui.frontend/packages/netherlands/src/App.js
@@ -1,4 +1,4 @@
-import {withPageHook, App} from "./setupApp";
+import {withPageHook, App} from "../../core/src/setupApp";
+import store from "@lebara/netherlands/redux/store";
 import { withModel } from "@adobe/aem-react-editable-components";
-import store from "@lebara/core/store";
 export default withModel(withPageHook(App, store));
